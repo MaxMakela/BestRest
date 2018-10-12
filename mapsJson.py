3619dbe00fa8eb11d0afd36bc6ef598423e67b23
max <max@gmail.com>
2018-10-12 21:34:36 +0300

    test many pages list

--- mapsJson.py
+++ mapsJson.py
@@ -26,13 +26,24 @@ if __name__ == '__main__':
 
     loc = result[0]['geometry']['location']
     pl = gmaps.places_nearby(loc, 5000, type='restaurant')
-    #time.sleep(2)
+    rest_list=pl['results']
 
-    best = get_best_restaurant(pl['results'])
+    if 'next_page_token' in pl:
+        while 'next_page_token' in pl:
+                time.sleep(2)
+                #print(pl['next_page_token'])
+                pl = gmaps.places_nearby(loc, 5000, type='restaurant', page_token=pl['next_page_token'])
+                #print(pl)
+                rest_list.append(pl['results'])
+
+    #print(rest_list)
+
+    print(len(rest_list))
+    best = get_best_restaurant(rest_list)
     print(best['name'] + ' ' + str(best['rating']))
-
     # вывод всего списка имен
-    # n=0
-    # while n<=(len(pl['results'])-1):
-    #     print(str(n+1)+'.'+pl['results'][n]['name']+' '+str(pl['results'][n]['rating']))
-    #     n+=1
+
+    #n = 0
+    #while n<=(len(rest_list)-1):
+    #    print(str(n+1)+'.'+rest_list[n]+' '+str(rest_list[n]['rating']))
+    #    n += 1
